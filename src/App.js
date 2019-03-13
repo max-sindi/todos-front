894eaae9b28d6cba70d2337137dfbaf59bcd00ea
max-sindi <britva.okkamaa@gmail.com>
2019-03-13 11:23:12 +0200

    develop branch created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>azaz</Fragment>
+    );
+  }
+}
+
+export default App;
