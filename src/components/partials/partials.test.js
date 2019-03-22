71298c3503389e2a6f7df15823d8b3c2c0b97973
max-sindi <britva.okkamaa@gmail.com>
2019-03-22 19:01:35 +0200

    stage

--- src/components/partials/partials.test.js
+++ src/components/partials/partials.test.js
@@ -1,12 +1,20 @@
+import React from 'react'
 import { Search } from './'
 import renderer from 'react-test-renderer'
+import {render, fireEvent, cleanup, waitForElement} from 'react-testing-library'
 import store from 'store/store'
 import { Provider } from 'react-redux'
 
+afterEach(cleanup)
+
 test('Search renders correctly', () => {
-  const component = rendered.create(
-    <Provider>
+  const component = renderer.create(
+    <Provider store={store}>
       <Search />
     </Provider>
   )
+
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+
 })
